87d32709cc87618327995b788efc6b32aade33a2
kunalpatel <kunalpatel446@gmail.com>
2017-02-18 11:05:05 -0500

    Tabs > Spaces

--- ai/main.py
+++ ai/main.py
@@ -3,15 +3,16 @@ import json
 from pprint import pprint
 
 with open('stocks.json') as data_file:
-    data = json.load(data_file)
+	data = json.load(data_file)
 
 weekly_changes = {}
 for tick in data:
-    weekly_changes[tick] = []
-    for days in 
-    days = data[tick]
-    net = NeuralNetwork(tick, days, [len(days), 5, 1])
-    net.SGD(30, len(days) / 100, 2)
+	weekly_changes[tick] = []
+	for days in 
+	days = data[tick]
+	net = NeuralNetwork(tick, days, [len(days), 5, 1])
+	net.SGD(30, len(days) / 100, 2)
 
 def geomean(nums):
-    return (reduce(lambda x, y: x*y, nums))**(1.0/len(nums))
+	return (reduce(lambda x, y: x*y, nums))**(1.0/len(nums))
+
