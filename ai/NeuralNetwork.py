501fd97c45b1a945291dcef70a73ade29ba22ee6
Abhinav Madahar <abhinavmadahar@gmail.com>
2017-02-18 22:47:02 -0500

    Improve NeuralNetwork accuracy

--- ai/NeuralNetwork.py
+++ ai/NeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import shuffle
+from math import sqrt
 
 sigmoid = lambda z: 1 / (1 + np.exp(-z))
 sigmoidprime = lambda z: sigmoid(z) * (1 - sigmoid(z)) # derivate of sigmoid function
@@ -9,6 +10,8 @@ prod = lambda iterable: reduce(lambda a, b: a * b, iterable, 1)
 geomean = lambda data: prod(data) ** (1 / len(data))
 arthmean = lambda data: sum(data) / len(data)
 
+distance_between = lambda a, b: sqrt(sum((a[j] - b[j]) ** 2 for j in xrange(len(a))))
+
 def chunk(array, n):
     chunks = []
     for i in xrange(0, len(array), n):
@@ -16,16 +19,14 @@ def chunk(array, n):
     return chunks
 
 class NeuralNetwork(object):
-    # opens is a list of opening prices in chronological order for the given stock
-    def __init__(self, opens):
-        self.opens = opens
+    # prices is a list of priceing prices in chronological order for the given stock
+    # sizes is the size of each layer in the network
+    def __init__(self, prices, sizes):
+        self.prices = prices
 
-        sizes = [5, 5, 2] # size of each layer in the network
         self.sizes = sizes
         self.biases = [np.random.randn(y) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-
-        self.train(100, 1)
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
@@ -36,14 +37,15 @@ class NeuralNetwork(object):
     # applies gradient descent on the entire data set for that stock at once because it's small
     # epochs is the number of times to apply gradient descent
     def train(self, epochs, learning_rate):
-        for _ in xrange(epochs): # for epochs number of times
+        learning_rate = float(learning_rate) # sorry for the type-casting lol
+        for n in xrange(epochs): # for epochs number of times
             nabla_b = [np.zeros(b.shape) for b in self.biases] # grad. of cost w/ respect to bias
             nabla_w = [np.zeros(w.shape) for w in self.weights] # grad. of cost w/ respect to weight
 
             # calculate y using the weekly percent changes
             # if the weekly_percent_change was positive
-            opens = self.opens
-            weeks = chunk(opens, 5)
+            prices = self.prices
+            weeks = chunk(prices, self.sizes[0])
             for i, week in enumerate(weeks[:-1]):
                 next_week = weeks[i+1]
                 change_in_average_value = arthmean(next_week) / arthmean(week) - 1
@@ -57,8 +59,11 @@ class NeuralNetwork(object):
 
                 # calculate and add the necessary changes to bias and weight
                 delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-                nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-                nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+                nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+                nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+            self.weights = [w - (learning_rate / len(self.prices)) * nw for w, nw in zip(self.weights, nabla_w)]
+            self.biases = [b - (learning_rate / len(self.prices)) * nb for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -76,31 +81,64 @@ class NeuralNetwork(object):
 
         # now go back
 
-        # dCda(output, y) is the gradient of the cost function with respect to the output
-        # a = sigmoid(z)
-        # dC/da * da/dz = dC/da * d/dz(sigmoid(z)) = dC/da * sigmoid'(z) = dC/dz = delta
-        # delta is a matrix in which delta[l][j] is the partial derivative of cost with respect to
-        # the weighed input, z, at the jth neuron of the lth layer
-        output = activations[-1]
-        delta = self.dCda(output, y) * sigmoidprime(zs[-1])
-        nabla_b[-1] = delta # BP3
-        nabla_w[-1] = np.dot(delta, output) # BP4
+        # let's do this literately
+        # backpropagation is all about delta, the partial derivate of C with respect to z[l][j]
+        # z[l][j] is the weighed input (w[l][j] dot a + b) of the jth neuron of the lth layer
+        # a[l][j] = sigmoid(w[l][j] dot a + b)
+        # a[l][j] = sigmoid(z[l][j])
+        # we will now use (BP1) to find delta
+        # we will refer to the partial derivate of C with respect to a[l][j] as dC/da
+        # dC/dz = dC/da * da/dz
+        # delta = dC/da * d/dz(sigmoid(z))
+        # delta = dC/da * sigmoid'(z)
+        # we can calculate dC/da with self.dCda and we have the sigmoidprime function already
+        # now, we can calculate delta
+        dCda = self.dCda(activations[-1], y)
+        dadz = sigmoidprime(zs[-1])
+        delta = dCda * dadz
+
+        # if b is the bias of a given neuron with weighed input z,
+        # dz/db = 1
+        # dC/db = dC/dz * dz/db
+        # dC/db = dC/dz * 1
+        # dC/db = dC/dz
+        # dC/db = delta (i.e. BP3)
+        # apply this for all the neurons in a given layer to get nabla_b, the gradient of cost with
+        # respect to the biases of the neurons in the current layer, the output layer
+        nabla_b[-1] = delta
+
+        # we can now calculate the gradient of cost with respect to the weights of the output layer
+        # self.weights[l][j][k] is the weight of an input from k to j, but error (delta) propagates
+        # backwards through the neural network, so we want the weight from j to k, which we can
+        # find by transposing self.weights[l] (i.e. np.transpose(self.weights[l]))
+        # because we're trying to found the gradient of the cost with respect to the weight of the
+        # connections going to the output layer, -1, we need to find nabla_w[-1]
+        # nabla_w[l][j][k] = a[l-1][k] x delta[l][j]
+        # where a[l-1][k] is the vector of activations of the neurons in the previous layer
+        nabla_w[-1] = np.matmul([[d] for d in delta], [activations[-2]])
 
         for l in xrange(2, len(self.sizes)):
             z = zs[-l]
             zp = sigmoidprime(z) # z'
             delta = np.dot(np.transpose(self.weights[-l+1]), delta) * zp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, np.transpose(activations[-l-1]))
+            nabla_w[-l] = np.matmul([[d] for d in delta], [activations[-l-1]])
+
         return (nabla_b, nabla_w)
 
+        #/literate python
+
+    def decision(self, inputs):
+        output = self.feedforward(inputs)
+        return 0 if output[0] > output[1] else 1
+
     def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+        cost = 0
+        for x, y in test_data:
+            output = self.feedforward(x)
+            decision = (output[1] - output[0] + 1) / 2
+            cost += (decision - y) ** 2
+        return cost / len(test_data)
 
     def dCda(self, output_activations, y):
         return output_activations - y
--- ai/main.py
+++ ai/main.py
@@ -1,10 +1,23 @@
-from NeuralNetwork import NeuralNetwork
+from NeuralNetwork import NeuralNetwork, chunk, arthmean
 import json
 
 with open('stocks.json') as data_file:
 	data = json.load(data_file)
 
-days = data["YHOO"]
-opens = [day["open"] for day in days]
-net = NeuralNetwork(opens)
-print(net.feedforward(opens[0:5]))
+days = data["PYPL"]
+opens = [day["high"] for day in days]
+
+input_size = 10
+testing_data = []
+weeks = chunk(opens, input_size)
+for i, week in enumerate(weeks[:-1]):
+    y = 0 if max(weeks[i+1]) > max(week) else 1
+    testing_data.append((week, y))
+
+net = NeuralNetwork(opens, [input_size, 5, 2])
+
+while sum(int(net.decision(x) == y) for x, y in testing_data) / float(len(testing_data)) < 0.5:
+    net.train(10, 0.1)
+
+p_correct = sum(int(net.decision(x) == y) for x, y in testing_data) / float(len(testing_data))
+print p_correct
